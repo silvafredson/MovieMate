bd86a3963115b355bb9e85f5d2a0713bb4ac4404
FREDSON SILVA <silva.fredson@hotmail.com>
2024-07-13 13:48:52 -0300

    Remove Storyboard and setup UIKit Programmatically

---
+++ MovieMate/MovieMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMate
+//
+//  Created by Fredson Silva on 13/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
